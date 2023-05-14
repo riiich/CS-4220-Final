42646fd0da854aff73ecc4c1e0fb0b77f17a442a
Rich <horich21@gmail.com>
2023-05-14 15:28:37 -0700

    update

--- recipe-application/routes/history.js
+++ recipe-application/routes/history.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-const { query } = require('express');
 const database = require('../db');
 
 // return json of all the searches in the database
@@ -20,18 +19,5 @@ router.get('/', async (req, res) => {
         res.status(500).json(error.toString());
     }
 });
-
-// return json of all the searches associated with query parameter
-// router.get('/:searchTerm', async (req, res) => {
-//     try {
-//         const { params } = req;
-//         const { searchTerm } = params;
-
-//         const results = await database.find('Results', searchTerm);
-//         res.json(results);
-//     } catch (error) {
-//         res.status(500).json(error.toString());
-//     }
-// });
 
 module.exports = router;
